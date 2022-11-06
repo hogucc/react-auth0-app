7f2da6e437ac64586f7e4e5de65795be8a2af0d9
hogucc <uku.h1r8@gmail.com>
2022-11-06 12:32:12 +0900

    React+TypeScript+Auth0でログイン機能を実装

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const envDomain : string = (process.env.REACT_APP_AUTH0_DOMAIN as string);
+const envClientID : string = (process.env.REACT_APP_AUTH0_CLIENT_ID as string);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider
+        domain={envDomain}
+        clientId={envClientID}
+        redirectUri={window.location.origin}
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
